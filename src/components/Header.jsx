fb1cfa0c7dfa9bf51ce1584f77df91c7ae0ae4f9
Frogodrig <72153510+Frogodrig@users.noreply.github.com>
2022-06-26 22:24:21 +0530

    deleted cache

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
       <h1>
         <CalendarTodayIcon fontSize="extra-large" />
         &nbsp;CP Calendar
-        <a className={'header-links'} href={"https://github.com/Frogodrig"} target={"_blank"} rel={'noreferrer'}><GitHubIcon fontSize="large" /></a>
+        <a className={'header-links'} href={"https://github.com/Frogodrig/CPC-Competitive-Programming-Calendar"} target={"_blank"} rel={'noreferrer'}><GitHubIcon fontSize="large" /></a>
         <a className={'header-links'} href="https://www.linkedin.com/in/aditya-awasthi-5b76481b6/" target={"_blank"} rel={'noreferrer'}><LinkedInIcon fontSize="extra-large" /></a>
       </h1>
     </header>
